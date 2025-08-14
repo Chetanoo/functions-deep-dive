bba36fc9fa9acdea5d78f503244ffbe3cdb6e7d5
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-13 23:29:32 -0300

    closures

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import "fmt"
 func main() {
 	numbers := []int{1, 2, 3}
 
+	double := createTransformers(2)
+
+	triple := createTransformers(3)
+
 	transformed := transformNumbers(&numbers, func(num int) int {
 		return num * 2
 	})
 
-	fmt.Println(transformed)
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
+	fmt.Println(transformed, doubled, tripled)
 }
 
 func transformNumbers(numbers *[]int, transform func(int) int) []int {
@@ -20,4 +27,10 @@ func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	}
 
 	return dNumbers
+}
+
+func createTransformers(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
