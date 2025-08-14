34cb439e59cf163de1973d87df1f8e4e7173612f
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-13 23:08:27 -0300

    function that returns function

--- main.go
+++ main.go
@@ -6,11 +6,19 @@ type transformerFunction func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
+	moreNumbers := []int{5, 6, 7, 8, 9}
 	doubled := transformNumbers(&numbers, double)
 	fmt.Println(doubled)
 
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(tripled)
+
+	transformer1 := getTransFormer(&numbers)
+	transformer2 := getTransFormer(&moreNumbers)
+	test1 := transformNumbers(&numbers, transformer1)
+	test2 := transformNumbers(&moreNumbers, transformer2)
+	fmt.Println(test1)
+	fmt.Println(test2)
 }
 
 func transformNumbers(numbers *[]int, transformer transformerFunction) []int {
@@ -19,6 +27,13 @@ func transformNumbers(numbers *[]int, transformer transformerFunction) []int {
 		dNumbers[index] = transformer(number)
 	}
 	return dNumbers
+}
+
+func getTransFormer(numbers *[]int) transformerFunction {
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
 }
 
 func double(number int) int {
