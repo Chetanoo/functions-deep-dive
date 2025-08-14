2cea352f86cdb2d4b44f069460e0b02a3e18dc01
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-13 23:18:43 -0300

    anonymous functions

---
+++ functions_are_values/functions_are_values.go
@@ -0,0 +1,45 @@
+package functions_are_values
+
+import "fmt"
+
+type transformerFunction func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+	moreNumbers := []int{5, 6, 7, 8, 9}
+	doubled := transformNumbers(&numbers, double)
+	fmt.Println(doubled)
+
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(tripled)
+
+	transformer1 := getTransFormer(&numbers)
+	transformer2 := getTransFormer(&moreNumbers)
+	test1 := transformNumbers(&numbers, transformer1)
+	test2 := transformNumbers(&moreNumbers, transformer2)
+	fmt.Println(test1)
+	fmt.Println(test2)
+}
+
+func transformNumbers(numbers *[]int, transformer transformerFunction) []int {
+	dNumbers := make([]int, len(*numbers))
+	for index, number := range *numbers {
+		dNumbers[index] = transformer(number)
+	}
+	return dNumbers
+}
+
+func getTransFormer(numbers *[]int) transformerFunction {
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
--- main.go
+++ main.go
@@ -2,44 +2,22 @@ package main
 
 import "fmt"
 
-type transformerFunction func(int) int
-
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
-	moreNumbers := []int{5, 6, 7, 8, 9}
-	doubled := transformNumbers(&numbers, double)
-	fmt.Println(doubled)
+	numbers := []int{1, 2, 3}
 
-	tripled := transformNumbers(&numbers, triple)
-	fmt.Println(tripled)
+	transformed := transformNumbers(&numbers, func(num int) int {
+		return num * 2
+	})
 
-	transformer1 := getTransFormer(&numbers)
-	transformer2 := getTransFormer(&moreNumbers)
-	test1 := transformNumbers(&numbers, transformer1)
-	test2 := transformNumbers(&moreNumbers, transformer2)
-	fmt.Println(test1)
-	fmt.Println(test2)
+	fmt.Println(transformed)
 }
 
-func transformNumbers(numbers *[]int, transformer transformerFunction) []int {
-	dNumbers := make([]int, len(*numbers))
-	for index, number := range *numbers {
-		dNumbers[index] = transformer(number)
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
 	}
+
 	return dNumbers
-}
-
-func getTransFormer(numbers *[]int) transformerFunction {
-	if (*numbers)[0] == 1 {
-		return double
-	}
-	return triple
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
