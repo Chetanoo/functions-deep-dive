e0f12a64b4a81218e630bdb83d7873ae23800380
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-13 23:39:47 -0300

    recursion in go

--- main.go
+++ main.go
@@ -3,34 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3}
-
-	double := createTransformers(2)
-
-	triple := createTransformers(3)
-
-	transformed := transformNumbers(&numbers, func(num int) int {
-		return num * 2
-	})
-
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
-
-	fmt.Println(transformed, doubled, tripled)
+	fact := factorial(5)
+	fmt.Println(fact)
 }
 
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
-	dNumbers := []int{}
-
-	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transform(val))
+func factorial(number int) int {
+	if number == 0 {
+		return 1
 	}
-
-	return dNumbers
-}
-
-func createTransformers(factor int) func(int) int {
-	return func(number int) int {
-		return number * factor
-	}
+	return number * factorial(number-1)
 }
