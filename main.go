bc89925c13dc65c348c2c96e85f80a2edadfc61e
KirillGorinDataArt <chetanvse@gmail.com>
2025-08-13 23:02:17 -0300

    functions as first class values, callback

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+type transformerFunction func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+	doubled := transformNumbers(&numbers, double)
+	fmt.Println(doubled)
+
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(tripled)
+}
+
+func transformNumbers(numbers *[]int, transformer transformerFunction) []int {
+	dNumbers := make([]int, len(*numbers))
+	for index, number := range *numbers {
+		dNumbers[index] = transformer(number)
+	}
+	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }
